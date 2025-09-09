4faf58656fff5056066fd815268bccf414c06b57
Shyama Devi <shyamadevi407@gmail.com>
2025-09-09 09:31:17 +0530

    sub

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,28 +1,33 @@
 <?php
 class User extends CI_Controller{
 
-	public function user_view($page='',$data=''){
-		if(isset($_SESSION['users2_id'])){
-			$id['user_id']=$_SESSION['users2_id'];
-			$navdata['userdata']=$this->My_model->select_where('users',$id)[0];
-		}
+    public function user_view($page = '', $data = []){
+        $navdata = [];
 
-		$this->load->view('user/navbaar',$navdata='');
-		$this->load->view("user/$page",$data='');
-		$this->load->view('user/footer');
-	}
-	function index(){
-		$this->user_view('index');
-	}
+        if(isset($_SESSION['users2_id'])){
+            $id['user_id'] = $_SESSION['users2_id'];
+            $navdata['userdata'] = $this->My_model->select_where('users', $id)[0];
+        }
 
-	function cart()
-	{
-		$this->user_view('cart');
-	}
+        $this->load->view('user/navbaar', $navdata);
+        $this->load->view("user/$page", $data);
+        $this->load->view('user/footer');
+    }
 
-	function checkout()
-	{
-		$this->user_view('checkout');
-	}
+    public function index(){
+        $this->user_view('index');
+    }
+
+    public function cart(){
+        $this->user_view('cart');
+    }
+
+    public function checkout(){
+        $this->user_view('checkout');
+    }
+
+    public function sub(){
+        $this->load->view('user/sub');
+    }
 }
 ?>
